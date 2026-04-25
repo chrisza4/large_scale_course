13e9b893ba152dd4a1cd368765e6e223bcd5baef
Chris <chakrit.lj@gmail.com>
2026-04-25 09:34:17 +0700

    Add more

---
+++ misc/checkout.js
@@ -0,0 +1,20 @@
+function checkout(cartId) {
+  // Pay and update stock
+}
+
+// Usage
+checkout(cartId);
+
+// --------------------------
+
+function pay(cartId) {
+  // Pay
+}
+
+function updateStock(cartId) {
+  // Update stock
+}
+
+// Usage
+pay(cartId);
+updateStock(cartId);
